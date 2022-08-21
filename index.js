8add8a52df83b18dd66b1b7052899f1e87df7916
Prakash Gupta <96722484+prakash817@users.noreply.github.com>
2022-08-21 11:56:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,18 +4,19 @@ const dotenv = require("dotenv").config();
 const mongoose = require("mongoose");
 const todoRoute = require("./routes/todo");
 const cors = require("cors");
+const PORT = process.env.PORT || 5000
 app.use(cors());
 app.use(express.json());
 
 //route midlewere
 app.use("/todo", todoRoute);
 
-app.listen(5000, async () => {
+app.listen(PORT, async () => {
   try {
     await mongoose.connect(process.env.MONGO_URL);
     console.log("db Succefully connected ");
   } catch (error) {
     console.log(error);
   }
-  console.log("server started on port http://localhost:5000");
+  console.log(`server started on port http://localhost:${PORT}`);
 });
